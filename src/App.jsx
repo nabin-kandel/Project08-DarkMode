aab7adbad3bf357f3209f3aa89b5b52e432a1cb1
“Nabin-Kandel” <nabinkandel@hotmail.com>
2023-11-19 21:02:10 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Navbar from './assets/components/Navbar'
+import Body from './assets/components/Body'
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Body />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+    <img src="reactjs-icon.png" className="nav--icon" />
+    <h3 className="nav--logo_text">ReactFacts</h3>
+    <h4 className="nav--">React Course - Project 1</h4>
+</nav>
+  )
+}
+
+export default Navbar
